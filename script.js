d093668c61ceca4847b3ceb133460c9ff37478d7
Madeline Carr <Carrm2@vcu.edu>
2018-10-09 13:08:58 -0400

    The game will restart
    The game will now restart when someone wins or when the restart button is clicked. Also there is a bit more added to the CSS.

--- script.js
+++ script.js
@@ -8,14 +8,30 @@ function makeVar(){
   var response = document.getElementById("guess").value;
   //Console logs response
   console.log(response);
-
+  // Read the Value and console log a response.
     if (response < randomNumber) {
       console.log("It is higher than that!!!");
     } else if (response > randomNumber) {
       console.log("It is lower than that!!!");
     } else if (response == randomNumber) {
       console.log("YOU WIN!!!");
+      startAgain();
     }else {
       console.log("Please enter a number!!!");
     }
 };
+
+function startAgain() {
+  randomNumber = Math.floor(Math.random()*10)+1;
+  console.log(randomNumber);
+}
+
+//Add enviroment backgrounds.
+//Pick level difficulty.
+//Change responses.
+//
+//
+//
+//
+//
+//
